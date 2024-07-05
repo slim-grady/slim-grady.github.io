0a87c68594c00bbe7d7245f780e8de1c6f47ae63
slim-grady <grady.baker6@gmail.com>
2024-07-05 17:52:06 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/champlain.png") {
+    myImage.setAttribute("src", "images/champlain2.png");
+  } else if (mySrc === "images/champlain2.png"){
+    myImage.setAttribute("src", "images/champlain3.png");
+  } else {
+    myImage.setAttribute("src", "images/champlain.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
